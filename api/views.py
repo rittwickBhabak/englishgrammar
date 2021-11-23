cc10bbc22ce502f4760af8ed901ba0e3d763ff49
Rittwick Bhabak <rittwick2019@gmail.com>
2021-11-23 19:20:18 +0530

    api permission added

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics 
+from rest_framework import generics, permissions
 from learn.models import Question
 from .serializers import QuestionSerializer 
 
 class QuestionList(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
